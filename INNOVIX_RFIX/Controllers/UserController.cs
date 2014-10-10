b8b72bf237190182eeaa986cd4086447ccc785cd
MarioEugenio <mario.eugenio@gmail.com>
2014-10-09 21:04:41 -0300

    [UPDATE]

--- INNOVIX_RFIX/Controllers/UserController.cs
+++ INNOVIX_RFIX/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Innovix.Base.Infrastructure.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
